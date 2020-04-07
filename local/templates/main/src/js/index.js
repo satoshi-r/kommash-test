2697a4f2c17ba318c987d33546e317a8aa791fb5
satoshi-r <rgeargerge@yandex.ru>
2020-04-07 18:29:42 +0300

    debug

--- local/templates/main/src/js/index.js
+++ local/templates/main/src/js/index.js
@@ -9,16 +9,6 @@ import './product'
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Остановить анимацию во время изменения размера окна
-    let resizeTimer;
-    window.addEventListener("resize", () => {
-        document.body.classList.add("resize-animation-stopper");
-        clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(() => {
-            document.body.classList.remove("resize-animation-stopper");
-        }, 400);
-    });
-
     // запрет на вытягивание картинок
     $("img, a").on("dragstart", function (event) { event.preventDefault(); });
 });
